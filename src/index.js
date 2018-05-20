3218dab98a40120734e1c77770d717a794e3f1fa
Christopher Best <bestchris@gmail.com>
2018-05-19 21:52:22 -0500

    initial app boilerplate

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+import { reactReduxFirebase, firebaseStateReducer } from 'react-redux-firebase';
+import { Router, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+import { createLogger } from 'redux-logger';
+import firebase from 'firebase';
+
+import routes from './routes';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyCKj_BRXYrNVGLbTlYtq517O7hxpPnZBZ8',
+    authDomain: 'crossword-dev.firebaseapp.com',
+    databaseURL: 'https://crossword-dev.firebaseio.com',
+    projectId: 'crossword-dev',
+    storageBucket: 'crossword-dev.appspot.com',
+    messagingSenderId: '960799145845',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const store = createStore(
+    combineReducers({
+        firebase: firebaseStateReducer,
+        routing: routerReducer,
+    }),
+    compose(
+        applyMiddleware(createLogger()),
+        reactReduxFirebase(firebase),
+    ),
+);
+
+const history = syncHistoryWithStore(browserHistory, store);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={history}>
+            {routes}
+        </Router>
+    </Provider>,
+    document.getElementById('react-root'),
+);
