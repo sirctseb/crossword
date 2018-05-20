6ec9f32320dc95309ca75890db0cf91695c898c4
Christopher Best <bestchris@gmail.com>
2018-05-19 21:52:22 -0500

    address crossowrds at /crosswords

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { IndexRoute, Route } from 'react-router';
+import { Route } from 'react-router';
 
 import App from './App';
 import Editor from './editor/Editor';
 
 export default (
     <Route path='/' component={App}>
-        <IndexRoute component={Editor} />
+        <Route path=':crosswordId' component={Editor} />
     </Route>
 );
