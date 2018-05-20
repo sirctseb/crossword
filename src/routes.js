35321814baa769e5123847069ca66469da4692dc
Christopher Best <bestchris@gmail.com>
2018-05-19 21:52:22 -0500

    establish the editor component

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { IndexRoute, Route } from 'react-router';
 
 import App from './App';
+import Editor from './editor/Editor';
 
 export default (
   <Route path='/' component={App}>
+    <IndexRoute component={Editor} />
   </Route>
 );
