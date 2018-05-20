f1536884c3ab60c520be6e95232b06b919264e8d
Christopher Best <bestchris@gmail.com>
2018-05-19 21:52:22 -0500

    remove redux cursor state management

--- src/editor/actions.js
+++
@@ -1,6 +0,0 @@
-import * as actionTypes from './actionTypes';
-
-export const setCursor = cursor => ({
-    type: actionTypes.SET_CURSOR,
-    cursor,
-});
--- src/editor/reducer.js
+++ src/editor/reducer.js
@@ -1,14 +1,8 @@
-import * as actionTypes from './actionTypes';
-
-
 const initialState = {
-    cursor: null,
 };
 
 export default (state = initialState, action) => {
     switch (action.type) {
-    case actionTypes.SET_CURSOR:
-        return { cursor: action.cursor };
     default:
         return state;
     }
