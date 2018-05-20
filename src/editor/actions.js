38550d4166012eb73860a1df31702140a6bde480
Christopher Best <bestchris@gmail.com>
2018-05-19 21:52:22 -0500

    add redux state for clue input

---
+++ src/editor/actions.js
@@ -0,0 +1,6 @@
+import * as actionTypes from './actionTypes';
+
+export const changeClue = clueInput => ({
+  type: actionTypes.CHANGE_CLUE,
+  clueInput,
+});
--- src/editor/reducer.js
+++ src/editor/reducer.js
@@ -1,8 +1,20 @@
+import update from 'immutability-helper';
+
+import * as actionTypes from './actionTypes';
+
 const initialState = {
+  clueInput: {
+    row: null,
+    column: null,
+    value: null,
+    direction: null,
+  },
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
+  case actionTypes.CHANGE_CLUE:
+    return update(state, { clueInput: { $set: action.clueInput } });
   default:
     return state;
   }
