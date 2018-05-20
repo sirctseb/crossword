7e280ff1f9dfaa2e6f17ee8d306bd34863818fa2
Christopher Best <bestchris@gmail.com>
2018-05-19 21:52:22 -0500

    add and render cursor state

--- src/editor/Editor.js
+++ src/editor/Editor.js
@@ -1,39 +1,55 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { compose } from 'redux';
+import { compose, bindActionCreators } from 'redux';
 import { firebaseConnect } from 'react-redux-firebase';
 import { get } from 'lodash';
 import { bemNamesFactory } from 'bem-names';
 
 import BoxControls from './BoxControls';
+import * as actions from './actions';
 
 const enhance = compose(
     firebaseConnect(props => ([
         `crosswords/${props.params.crosswordId}`,
     ])),
-    connect(({ firebase: { data: { crosswords } } }, props) => ({
-        crossword: crosswords && crosswords[props.params.crosswordId],
-        path: `crosswords/${props.params.crosswordId}`,
-    })),
+    connect(
+        ({ firebase: { data: { crosswords } }, editor }, props) => ({
+            crossword: crosswords && crosswords[props.params.crosswordId],
+            path: `crosswords/${props.params.crosswordId}`,
+            editor,
+        }),
+        dispatch => ({
+            actions: bindActionCreators(actions, dispatch),
+        }),
+    ),
 );
 
 class Editor extends Component {
     render() {
         const bem = bemNamesFactory('editor');
 
-        const { firebase: { set }, path, crossword } = this.props;
+        const {
+            firebase: { set }, path, crossword, editor,
+        } = this.props;
         if (!crossword) {
             return 'WAIT!';
         }
         const rows = [];
-        for (let i = 0; i < crossword.rows; i += 1) {
+        for (let row = 0; row < crossword.rows; row += 1) {
             const boxes = [];
-            for (let j = 0; j < crossword.rows; j += 1) {
-                const { blocked, circled, shaded } = get(crossword, `boxes.${i}.${j}`, {});
-                const boxPath = `${path}/boxes/${i}/${j}`;
+            for (let column = 0; column < crossword.rows; column += 1) {
+                const { blocked, circled, shaded } = get(crossword, `boxes.${row}.${column}`, {});
+                const focused = editor.cursor &&
+                    row === editor.cursor.row &&
+                    column === editor.cursor.column;
+                const boxPath = `${path}/boxes/${row}/${column}`;
+
                 boxes.push((
-                    <div className={bem('box', { blocked, circled, shaded })}
-                        key={`box-${i}-${j}`}>
+                    <div className={bem('box', {
+                        blocked, circled, shaded, focused,
+                    })}
+                    key={`box-${row}-${column}`}
+                    onClick={() => this.props.actions.setCursor({ row, column }) }>
                         <BoxControls onToggleBlock={() => set(`${boxPath}/blocked`, !blocked)}
                             onToggleCircle={() => set(`${boxPath}/circled`, !circled)}
                             onToggleShade={() => set(`${boxPath}/shaded`, !shaded)} />
@@ -42,7 +58,7 @@ class Editor extends Component {
             }
             rows.push((
                 <div className='editor__row'
-                    key={`row-${i}`}>
+                    key={`row-${row}`}>
                     {boxes}
                 </div>
             ));
