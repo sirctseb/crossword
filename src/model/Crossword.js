7fce07ae690c0a22b4d7d64bdbd87a82e1643cb0
Christopher Best <bestchris@gmail.com>
2018-08-21 23:45:35 -0400

    Move active calculation to crossword model

--- src/model/Crossword.js
+++ src/model/Crossword.js
@@ -1,4 +1,5 @@
 import { get } from 'lodash';
+import { ACROSS } from '../editor/constants';
 
 export default {
   acrossPattern: (crossword, row, column) => {
@@ -22,5 +23,37 @@ export default {
       down.push(get(crossword, `boxes.${i}.${column}.content`, '.'));
     }
     return down.join('');
+  },
+  isCursorAnswer: (row, column, box, crossword, cursor) => {
+    if (box.blocked) return false;
+    if (row === cursor.row && column === cursor.column) return true;
+
+    if (cursor.direction === ACROSS) {
+      if (row !== cursor.row) {
+        return false;
+      }
+
+      for (let increment = Math.sign(cursor.column - column), columnIter = column;
+        columnIter >= 0 && columnIter < crossword.rows && !get(crossword, `boxes.${row}.${columnIter}.blocked`);
+        columnIter += increment) {
+        if (columnIter === cursor.column) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    if (column !== cursor.column) {
+      return false;
+    }
+
+    for (let increment = Math.sign(cursor.row - row), rowIter = row;
+      rowIter >= 0 && rowIter < crossword.rows && !get(crossword, `boxes.${rowIter}.${column}.blocked`);
+      rowIter += increment) {
+      if (rowIter === cursor.row) {
+        return true;
+      }
+    }
+    return false;
   },
 };
