9acaabc76a072fe43a4d7063bc245c1ae44d805d
Christopher Best <bestchris@gmail.com>
2018-10-18 23:24:21 -0400

    Implement rebus input

--- src/model/Crossword.js
+++ src/model/Crossword.js
@@ -56,6 +56,7 @@ export default {
     }
     return false;
   },
+  isFocusBox: (row, column, cursor) => row === cursor.row && column === cursor.column,
   completeAnswers: (crossword) => {
     const MISSING_VALUE = { blocked: true };
     const coordsToSignifier = (row, column) => {
