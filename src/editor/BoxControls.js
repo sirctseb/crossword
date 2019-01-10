db05f7f1defe426b961e4028631077cd70c6d1d5
Christopher Best <bestchris@gmail.com>
2019-01-09 22:28:58 -0500

    Reduce prop changes and box rerenderings

--- src/editor/BoxControls.js
+++ src/editor/BoxControls.js
@@ -2,52 +2,44 @@ import React, { Component } from 'react';
 import propTypes from 'prop-types';
 import { bemNamesFactory } from 'bem-names';
 
-import UndoHistory from '../undo/UndoHistory';
-import FirebaseChange from '../undo/FirebaseChange';
-
 class BoxControls extends Component {
+  killEvent = (evt) => {
+    evt.preventDefault();
+    evt.stopPropagation();
+  }
+
+  handleToggleCircle = (evt) => {
+    this.props.onToggleAttribute('circled');
+    evt.stopPropagation();
+  }
+
+  handleToggleShade = (evt) => {
+    this.props.onToggleAttribute('shaded');
+    evt.stopPropagation();
+  }
+
   render() {
     const bem = bemNamesFactory('box-controls');
-    const { boxRef, box: { blocked, circled, shaded } } = this.props;
-    const undoHistory = UndoHistory.getHistory('crossword');
+    const {
+      box: {
+        blocked, circled, shaded,
+      },
+    } = this.props;
 
     return (
       <div className='box-controls'>
         <div className={bem('block', { blocked })}
-          onMouseDown={(evt) => {
-            evt.preventDefault();
-            evt.stopPropagation();
-          }}
+          onMouseDown={this.killEvent}
           onClick={this.props.onBlock} />
         {
           !blocked && <div className={bem('circle', { circled })}
-            onMouseDown={(evt) => {
-              evt.preventDefault();
-              evt.stopPropagation();
-            }}
-            onClick={(evt) => {
-              undoHistory.add(FirebaseChange.FromValues(
-                boxRef.child('circled'),
-                !circled,
-                circled,
-              ));
-              evt.stopPropagation();
-            }}/>
+            onMouseDown={this.killEvent}
+            onClick={this.handleToggleCircle}/>
         }
         {
           !blocked && <div className={bem('shade', { shaded })}
-            onMouseDown={(evt) => {
-              evt.preventDefault();
-              evt.stopPropagation();
-            }}
-            onClick={(evt) => {
-              undoHistory.add(FirebaseChange.FromValues(
-                boxRef.child('shaded'),
-                !shaded,
-                shaded,
-              ));
-              evt.stopPropagation();
-            }}/>
+            onMouseDown={this.killEvent}
+            onClick={this.handleToggleShade}/>
         }
       </div>
     );
@@ -55,9 +47,9 @@ class BoxControls extends Component {
 }
 
 BoxControls.propTypes = {
-  boxRef: propTypes.object.isRequired,
   box: propTypes.object.isRequired,
   onBlock: propTypes.func.isRequired,
+  onToggleAttribute: propTypes.func.isRequired,
 };
 
 export default BoxControls;
