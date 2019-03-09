bbb63bd8942dd833469f9fd5d4a12b7d79adb070
Christopher Best <bestchris@gmail.com>
2019-03-08 22:42:05 -0500

    Start preview list

---
+++ src/PreviewList.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firebaseConnect } from 'react-redux-firebase';
+import PropTypes from 'prop-types';
+
+import { getUserCrosswords } from './user/selectors';
+// import CrosswordPreview from './CrosswordPreview';
+const CrosswordPreview = ({ id }) => <div>{ id }</div>;
+
+const enhance = compose(
+  firebaseConnect(props => ([
+    `users/${props.userId}/crosswords`,
+  ])),
+  connect((state, props) =>
+    getUserCrosswords(state, props) ||
+        {
+          loading: true,
+        }),
+);
+
+class PreviewList extends Component {
+  render() {
+    return (
+      <div className='preview-list'>
+        {
+          Object.keys(this.props.crosswords).map(id =>
+            <CrosswordPreview id={id} key={id} />)
+        }
+      </div>
+    );
+  }
+}
+
+PreviewList.propTypes = {
+  crosswords: PropTypes.object,
+};
+
+export default enhance(PreviewList);
