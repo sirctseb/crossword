92abcad8ec653b9929d8b92ccd93e81f5736c4be
Christopher Best <bestchris@gmail.com>
2019-03-09 22:50:12 -0500

    start live previews

---
+++ src/CrosswordPreview.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firebaseConnect } from 'react-redux-firebase';
+import { bemNamesFactory } from 'bem-names';
+import { get } from 'lodash';
+
+import * as selectors from './editor/selectors';
+
+const bem = bemNamesFactory('crossword-preview');
+
+const enhance = compose(
+    firebaseConnect(({ id }) => ([
+        `/crosswords/${id}`,
+    ])),
+    connect((state, props) => ({
+        crossword: selectors.getCrossword(state, { crosswordId: props.id }),
+        metadata: props,
+    }))
+);
+
+const drawBoxes = ({ rows, boxes }) => {
+    const rowElements = [];
+    for (let row = 0; row < rows; row += 1) {
+        const boxElements = [];
+        for (let column = 0; column < rows; column += 1) {
+            boxElements.push(<div key={`box-${row}-${column}`}
+                className={bem('box', {
+                    blocked: get(boxes, [row, column, 'blocked']),
+                })} />);
+        }
+        rowElements.push(<div key={`row-${row}`} className={bem('row')}>{boxElements}</div>);
+    }
+    return rowElements;
+};
+
+export default enhance(({ metadata, crossword }) => (
+    <div className={bem()}>
+        {
+            crossword &&
+            <div className={bem('grid')}>
+                { drawBoxes(crossword) }
+            </div>
+        }
+        <a href={`/${metadata.id}`}>
+            {metadata.title || 'Untitled'}
+        </a>
+    </div>
+));
