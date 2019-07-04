19b144b377eaa2e8ea12d261a61969c146285902
Christopher Best <bestchris@gmail.com>
2019-07-03 22:03:26 -0400

    Move cursor publish hoc to separate file

---
+++ src/editor/publishCursor.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+export default Editor =>
+    class FocusSyncEditor extends Component {
+        state = {}
+
+        componentDidMount() {
+            // create cursor
+            const initialCursor = {
+                userId: this.props.firebase.auth().currentUser.uid,
+            };
+            const cursorRef = this.props.firebase.ref(`cursors/${this.props.params.crosswordId}`)
+                .push(initialCursor);
+
+            // set up onDelete to remove the cursor
+            cursorRef.onDisconnect().set(null);
+
+            this.setState({ cursorRef });
+        }
+
+        componentWillUnmount() {
+            // remove onDelete
+            this.state.cursorRef.onDisconnect().cancel();
+            // delete cursor
+            this.state.cursorRef.set(null);
+        }
+
+        handleBoxFocus = (cursor) => {
+            this.props.actions.setCursor(cursor, this.props.params.crosswordId);
+            this.state.cursorRef.update(cursor);
+        }
+
+        render() {
+            return <Editor {...this.props} onBoxFocus={this.handleBoxFocus} />;
+        }
+    };
