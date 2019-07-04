7bb3195f7e59c234eab83dc61d8cec62f366302a
Christopher Best <bestchris@gmail.com>
2019-07-03 21:47:16 -0400

    add cursor rules tests

--- rules.test.js
+++ rules.test.js
@@ -11,6 +11,7 @@ const firebase = require('@firebase/testing');
 const databaseName = 'crossword-dev';
 const alice = 'alice';
 const bob = 'bob';
+const charlie = 'charlie';
 
 const authedApp = auth => firebase.initializeTestApp({
     databaseName,
@@ -30,14 +31,6 @@ beforeEach(() => adminApp().ref().set(null));
 
 after(() => {
     Promise.all(firebase.apps().map(a => a.delete()));
-});
-
-describe('tests test', () => {
-    it('works', () => {
-        const app = authedApp({ uid: 'anything' });
-        const result = app.ref('secret').once('value');
-        return expect(result).to.be.rejected();
-    });
 });
 
 describe('crossword', () => {
@@ -87,5 +80,83 @@ describe('crossword', () => {
             },
             'permissions/cw-id': { owner: bob },
         })).to.be.rejected();
+    });
+});
+
+describe('cursor', () => {
+    describe('with cw in place', () => {
+        beforeEach(() =>
+            adminApp().ref().update({
+                'crosswords/cw-id': { rows: 15, symmetric: true, title: 'untitled' },
+                [`users/${alice}/crosswords/cw-id`]: {
+                    title: 'Untitled',
+                },
+                'permissions/cw-id': { owner: alice, collaborators: { bob: true } },
+            }));
+
+        it('can be created', () => {
+            const app = authedApp({ uid: alice });
+            return expect(app.ref().update({
+                'cursors/cw-id/cursor-id': {
+                    userId: alice,
+                },
+            })).to.be.fulfilled();
+        });
+
+        it('cannot be created under another users id', () => {
+            const app = authedApp({ uid: alice });
+            return expect(app.ref().update({
+                'cursors/cw-id/cursor-id': {
+                    userId: bob,
+                },
+            })).to.be.rejected();
+        });
+
+        describe('with existing cursor', () => {
+            beforeEach(() =>
+                adminApp().ref().update({
+                    'cursors/cw-id/cursor-id': {
+                        userId: alice,
+                        row: 0,
+                        column: 0,
+                    },
+                }));
+
+            it('cannot be deleted by another user', () => {
+                const app = authedApp({ uid: bob });
+                return expect(app.ref('cursors/cw-id/cursor-id').set(null)).to.be.rejected();
+            });
+
+            it('can be deleted by owner', () => {
+                const app = authedApp({ uid: alice });
+                return expect(app.ref('cursors/cw-id/cursor-id').set(null)).to.be.fulfilled();
+            });
+
+            it('can be read by owner', () => {
+                const app = authedApp({ uid: alice });
+                return expect(app.ref('cursors/cw-id/cursor-id').once('value')).to.be.fulfilled();
+            });
+
+            it('can be read by collaborator', () => {
+                const app = authedApp({ uid: bob });
+                return expect(app.ref('cursors/cw-id/cursor-id').once('value')).to.be.fulfilled();
+            });
+
+            it('cannot be read by non-owner non-collaborator', () => {
+                const app = authedApp({ uid: charlie });
+                return expect(app.ref('cursors/cw-id/cursor-id').once('value')).to.be.rejected();
+            });
+        });
+    });
+
+    describe('with no cw in place', () => {
+        it('cannot be created', () => {
+            const app = authedApp({ uid: alice });
+            return expect(app.ref().update({
+                'cursors/cw-id/cursor-id': {
+                    userId: alice,
+                },
+            })).to.be.rejected();
+        });
     });
 });
