2aad74d9f89d74d800a973e2ba6cbb5fa2fda9df
Christopher Best <bestchris@gmail.com>
2020-01-21 21:50:50 -0500

    Make Box a functional component

--- src/editor/Box.js
+++ src/editor/Box.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import { bemNamesFactory } from 'bem-names';
 import propTypes from 'prop-types';
 
@@ -6,113 +6,104 @@ import BoxControls from './BoxControls';
 import RebusInput from './RebusInput';
 
 const targetFocused = ({ currentTarget }) => document.activeElement === currentTarget;
+const bem = bemNamesFactory('box');
 
-export default class Box extends PureComponent {
-  constructor(props) {
-    super(props);
+const Box = ({
+  row,
+  column,
+  box,
+  box: {
+    blocked,
+    circled,
+    shaded,
+    content,
+  },
+  cursor,
+  cursorAnswer: active,
+  makeUndoableChange,
+  clueLabel,
+  onBlock,
+  onBoxFocus,
+  onAfterSetContent,
+}) => {
+  const [rebus, setRebus] = useState(false);
 
-    this.state = {
-      rebus: false,
-    };
-  }
+  const handleFocus = () => {
+    onBoxFocus({ row, column });
+  };
 
-  handleFocus = () => {
-    const { row, column } = this.props;
-    this.props.onBoxFocus({ row, column });
-  }
-
-  handleMouseDown = (evt) => {
-    if (this.props.cursor) {
-      this.setState({ rebus: true });
-      // when state goes to { rebus: true }, the input is rendered
+  const handleMouseDown = (evt) => {
+    if (cursor) {
+      setRebus(true);
+      // when rebus goes to true
       // and we focus the input element in RebusInput.componentDidMount
       // we have to preventDefault here so that we don't immediately
       // yank it back to the box
       evt.preventDefault();
     }
-  }
+  };
 
-  handleRebusClose = (content = this.props.box.content) => {
-    this.setContent(content);
-    this.setState({ rebus: false });
-  }
-
-  handleOnBlock = () => {
-    const { row, column, box: { blocked } } = this.props;
-    this.props.onBlock(row, column, !blocked);
-  }
-
-  handleToggleAttribute = (attribute) => {
-    const { row, column, box } = this.props;
-
-    this.props.makeUndoableChange(
-      `boxes/${row}/${column}/${attribute}`,
-      !box[attribute],
-      box[attribute],
-    );
-  }
-
-  setContent(newContent) {
-    const { row, column, box: { content } } = this.props;
-    this.props.makeUndoableChange(
+  const setContent = (newContent) => {
+    makeUndoableChange(
       `boxes/${row}/${column}/content`,
       newContent,
       content,
     );
-    this.props.onAfterSetContent(newContent);
-  }
+    onAfterSetContent(newContent);
+  };
 
-  render() {
-    const bem = bemNamesFactory('box');
-    const {
-      box: {
-        blocked, circled, shaded, content,
-      }, clueLabel, cursorAnswer: active,
-      row, column,
-    } = this.props;
-    const { rebus } = this.state;
+  const handleRebusClose = (newContent = content) => {
+    setContent(newContent);
+    setRebus(false);
+  };
 
-    return (
-      <div
-        className={
-          bem(
-            {
-              blocked, circled, shaded, active,
-            },
-            [`at-${row}-${column}`],
-          )}
-        tabIndex={!blocked ? '0' : undefined}
-        onKeyPress={(evt) => {
-          if (/^[A-Za-z]$/.test(evt.key) && targetFocused(evt)) {
-            this.setContent(evt.key);
-          }
-        }}
-        onKeyDown={(evt) => {
-          if (evt.key === 'Backspace' && targetFocused(evt)) {
-            this.setContent(null);
-          }
-        }}
-        onFocus={this.handleFocus}
-        onMouseDown={this.handleMouseDown}>
-        <BoxControls onToggleAttribute={this.handleToggleAttribute}
-          box={this.props.box}
-          onBlock={this.handleOnBlock} />
-        {
-          rebus &&
-                    <RebusInput
-                      content={content}
-                      onClose={this.handleRebusClose} /> }
-        {
-          clueLabel &&
-                    <div className={bem('clue-index')}>
-                      {clueLabel}
-                    </div>
-        }
-        { content }
-      </div>
+  const handleOnBlock = () => {
+    onBlock(row, column, !blocked);
+  };
+
+  const handleToggleAttribute = (attribute) => {
+    makeUndoableChange(
+      `boxes/${row}/${column}/${attribute}`,
+      !box[attribute],
+      box[attribute],
     );
-  }
-}
+  };
+
+  return (
+    <div
+      className={
+        bem(
+          {
+            blocked, circled, shaded, active,
+          },
+          [`at-${row}-${column}`],
+        )}
+      tabIndex={!blocked ? '0' : undefined}
+      onKeyPress={(evt) => {
+        if (/^[A-Za-z]$/.test(evt.key) && targetFocused(evt)) {
+          setContent(evt.key);
+        }
+      }}
+      onKeyDown={(evt) => {
+        if (evt.key === 'Backspace' && targetFocused(evt)) {
+          setContent(null);
+        }
+      }}
+      onFocus={handleFocus}
+      onMouseDown={handleMouseDown}>
+      <BoxControls onToggleAttribute={handleToggleAttribute}
+        box={box}
+        onBlock={handleOnBlock} />
+      {
+        rebus && <RebusInput content={content} onClose={handleRebusClose} />
+      }
+      {
+        clueLabel && <div className={bem('clue-index')}>{ clueLabel }</div>
+      }
+      { content }
+    </div>
+  );
+};
 
 Box.propTypes = {
   row: propTypes.number.isRequired,
@@ -124,9 +115,12 @@ Box.propTypes = {
     content: propTypes.string,
   }).isRequired,
   cursor: propTypes.bool.isRequired,
+  cursorAnswer: propTypes.bool.isRequired,
   makeUndoableChange: propTypes.func.isRequired,
   clueLabel: propTypes.number,
   onBlock: propTypes.func.isRequired,
   onBoxFocus: propTypes.func.isRequired,
   onAfterSetContent: propTypes.func.isRequired,
 };
+
+export default Box;
