a7ac6768ffb6572f09119f9b7ac4df238b4528e2
Christopher Best <bestchris@gmail.com>
2020-01-25 22:54:26 -0500

    Make ThemeEntryList a functional components

--- src/editor/themeEntries/ThemeEntryList.js
+++ src/editor/themeEntries/ThemeEntryList.js
@@ -1,27 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { bemNamesFactory } from 'bem-names';
 
 const bem = bemNamesFactory('theme-entry-list');
 
-class ThemeEntryList extends Component {
-  render() {
-    const { entries, onDelete } = this.props;
-    return (
-      <div className={bem()}>
-        {
-          entries.map(({ text, used }) =>
-            <div className={bem('entry')}
-              key={text}>
-              <div className={bem('text', { used })}>{ text }</div>
-              <div className={bem('delete')}
-                onClick={() => onDelete(text)}>x</div>
-            </div>)
-        }
-      </div>
-    );
-  }
-}
+const ThemeEntryList = ({ entries, onDelete }) => (
+  <div className={bem()}>
+    {
+      entries.map(({ text, used }) =>
+        <div className={bem('entry')}
+          key={text}>
+          <div className={bem('text', { used })}>{ text }</div>
+          <div className={bem('delete')}
+            onClick={() => onDelete(text)}>x</div>
+        </div>)
+    }
+  </div>
+);
 
 ThemeEntryList.propTypes = {
   entries: PropTypes.arrayOf(PropTypes.shape({
