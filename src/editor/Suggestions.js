c79490da8eaa423398f72c16b15c22218cc39e49
Christopher Best <bestchris@gmail.com>
2020-01-31 23:01:45 -0500

    Migrate Suggestions to use hooks

--- src/editor/Suggestions.js
+++ src/editor/Suggestions.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { bemNamesFactory } from 'bem-names';
 
-import * as selectors from './selectors';
+import { getAmendedSuggestions } from './selectors';
 
 const bem = bemNamesFactory('suggestions');
 
-const Suggestions = ({ suggestions: { across, down } }) => {
-  const renderSuggestions = suggestions =>
-    (suggestions.length > 0 ?
-      suggestions.map(suggestion => (
-        <div className={bem('suggestion')}
-          key={suggestion}>
-          {suggestion}
-        </div>
-      )) :
-      <div className={bem('no-suggestions')}>
-        no matches
-      </div>);
+const renderSuggestions = suggestions =>
+  (suggestions.length > 0 ?
+    suggestions.map(suggestion => (
+      <div className={bem('suggestion')}
+        key={suggestion}>
+        {suggestion}
+      </div>
+    )) :
+    <div className={bem('no-suggestions')}>
+      no matches
+    </div>);
+
+const Suggestions = () => {
+  const params = useParams();
+  const { across, down } = useSelector(state => getAmendedSuggestions(state, params));
 
   return (
     <div className={bem()}>
@@ -39,13 +41,4 @@ const Suggestions = ({ suggestions: { across, down } }) => {
   );
 };
 
-Suggestions.propTypes = {
-  suggestions: PropTypes.shape({
-    across: PropTypes.arrayOf(PropTypes.string).isRequired,
-    down: PropTypes.arrayOf(PropTypes.string).isRequired,
-  }).isRequired,
-};
-
-export default withRouter(connect((state, props) => ({
-  suggestions: selectors.getAmendedSuggestions(state, props.match.params),
-}))(Suggestions));
+export default Suggestions;
