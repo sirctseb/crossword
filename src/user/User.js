63059ffba72fc7ca5898503e39c7a63a0e973186
Christopher Best <bestchris@gmail.com>
2020-01-31 22:52:35 -0500

    Migrate User to use hooks

--- src/user/User.js
+++ src/user/User.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 import { bemNamesFactory } from 'bem-names';
-import Wait from '../Wait';
 
 import { getUserId } from './selectors';
 import PreviewList from './PreviewList';
@@ -11,8 +9,10 @@ import UserSection from './UserSection';
 
 const bem = bemNamesFactory('user');
 
-const User = ({ userId }) =>
-  <div className={bem()}>
+const User = () => {
+  const userId = useSelector(getUserId) || null;
+
+  return userId && <div className={bem()}>
     <UserSection>
       <PreviewList userId={userId}>
         My Crosswords
@@ -22,11 +22,6 @@ const User = ({ userId }) =>
       <WordList userId={userId} />
     </UserSection>
   </div>;
-
-User.propTypes = {
-  userId: PropTypes.string.isRequired,
 };
 
-export default connect(state => ({
-  userId: getUserId(state),
-}))(Wait(User));
+export default User;
