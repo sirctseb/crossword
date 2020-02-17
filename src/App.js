81da29549f2deac9d51d0d67908ee564b1265f97
Christopher Best <bestchris@gmail.com>
2020-02-17 10:43:16 -0500

    0689 fixup

--- src/App.js
+++ src/App.js
@@ -1,31 +1,6 @@
-import React, { Component, useState, memo } from 'react';
+import React, { Component } from 'react';
 
 import Header from './Header';
-
-const CountRenderer =
-// memo(
-  ({ count, children }) =>
-  <div>Count Renderer ({children}): { count }</div>
-  // )
-
-const RenderTest = () => {
-  const [count, setCount] = useState(0);
-  const [other, setOther] = useState(100);
-
-  return (
-    <div>
-      <div>count: { count }</div>
-      <CountRenderer count={count}>
-        'count'
-      </CountRenderer>
-      <CountRenderer count={other}>
-        'other'
-      </CountRenderer>
-      <button onClick={() => setCount(count + 1)}>increment count</button>
-      <button onClick={() => setOther(other + 1)}>increment other</button>
-    </div>
-  );
-};
 
 class App extends Component {
   render() {
@@ -33,7 +8,6 @@ class App extends Component {
       <div className='app'>
         <Header />
         {this.props.children}
-        <RenderTest />
       </div>
     );
   }
