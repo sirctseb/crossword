f073a867a7bd4d0ae085e0a1ea05f3bf08de71eb
Christopher Best <bestchris@gmail.com>
2020-03-22 20:08:19 -0400

    Implement communal crossword finish function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
 
 exports.helloWorld = functions.https.onCall(() => 'hello world');
 
@@ -19,3 +22,32 @@ exports.decorateCursor = functions.database.ref('/cursors/{crosswordId}/{cursorI
     displayName: context.auth.displayName,
     photoUrl: context.auth.photoUrl,
   }));
+
+const snapVal = snap => snap.val();
+
+exports.finishCommunalCrossword = functions.https.onCall(() =>
+  admin.database().ref('/communalCrossword/current').once('value').then(snapVal)
+    .then(currentId => admin.database().ref('/crosswords').child(currentId).once('value')
+      .then((crosswordSnap) => {
+        const crossword = crosswordSnap.val();
+        const { rows } = crossword;
+        const range = [...Array(rows).keys()];
+        if (range.every(row => range.every((column) => {
+          if (crossword.boxes.hasOwnProperty(row) && crossword.boxes[row].hasOwnProperty(column)) {
+            const { content, blocked } = crossword.boxes[row][column];
+            return content || blocked;
+          }
+          return false;
+        }))) {
+          const ref = admin.database().ref();
+          const newCrosswordId = ref.push().key;
+          return ref.update({
+            [`/permissions/${currentId}/readonly`]: true,
+            [`/communalCrossword/archive/${currentId}`]: true,
+            '/communalCrossword/current': newCrosswordId,
+            [`/crosswords/${newCrosswordId}`]: { rows: 15, symmetric: true, title: `Puzzle - ${Date()}` },
+            [`/permissions/${newCrosswordId}/global`]: true,
+          });
+        }
+        throw new Error('Crossword not complete');
+      })));
