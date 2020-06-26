9e6dea6393289b53ee10679f6117a2ea8d91ea17
Christopher Best <bestchris@gmail.com>
2020-06-25 23:33:16 -0400

    Decorate cursors with colors

--- functions/decorateCursor.test.js
+++ functions/decorateCursor.test.js
@@ -29,10 +29,35 @@ describe('Cursor Decoration', () => {
     });
 
     it('adds a user\'s photo url to the cursor', () => {
-      const newCursor = test.database.makeDataSnapshot({ userid: aliceAuth.uid, row: 0, colum: 0 }, '/cursor/cw-id/cursor-id');
+      const newCursor = test.database.makeDataSnapshot({ userid: aliceAuth.uid, row: 0, colum: 0 }, '/cursors/cw-id/cursor-id');
       return wrapped(newCursor, aliceContext).then(() =>
         admin.database().ref('/cursors/cw-id/cursor-id/photoUrl').once('value').then(createdSnap =>
           expect(createdSnap.val()).to.eq(aliceAuth.photoUrl)));
+    });
+
+    describe('colors', () => {
+      it('adds a color to the cursor', () => {
+        const newCursor = test.database.makeDataSnapshot({ userid: aliceAuth.uid, row: 0, colum: 0 }, '/cursors/cw-id/cursor-id');
+        return wrapped(newCursor, aliceContext).then(() =>
+          admin.database().ref('/cursors/cw-id/cursor-id/color').once('value').then(createdSnap =>
+            expect(createdSnap.val()).to.eq('FFFF00')));
+      });
+
+      it('adds a different color to a second cursor', () =>
+        admin.database().ref('/cursors/cw-id').set({ 'cursor-id': { color: 'FFFF00' } }).then(() => {
+          const newCursor = test.database.makeDataSnapshot({ userid: aliceAuth.uid, row: 0, colum: 0 }, '/cursors/cw-id/cursor-id2');
+          return wrapped(newCursor, aliceContext).then(() =>
+            admin.database().ref('/cursors/cw-id/cursor-id2/color').once('value').then(createdSnap =>
+              expect(createdSnap.val()).to.eq('1CE6FF')));
+        }));
+
+      it('reuses colors from cursors that have been deleted', () =>
+        admin.database().ref('/cursors/cw-id').set({ 'cursor-id': { color: '1CE6FF' } }).then(() => {
+          const newCursor = test.database.makeDataSnapshot({ userid: aliceAuth.uid, row: 0, colum: 0 }, '/cursors/cw-id/cursor-id3');
+          return wrapped(newCursor, aliceContext).then(() =>
+            admin.database().ref('/cursors/cw-id/cursor-id3/color').once('value').then(createdSnap =>
+              expect(createdSnap.val()).to.eq('FFFF00')));
+        }));
     });
   });
 });
--- functions/index.js
+++ functions/index.js
@@ -14,17 +14,40 @@ const words = [
   'cab',
 ];
 
+const colors = [
+  'FFFF00', '1CE6FF', 'FF34FF', 'FF4A46', '008941', '006FA6', 'A30059',
+  'FFDBE5', '7A4900', '0000A6', '63FFAC', 'B79762', '004D43', '8FB0FF', '997D87',
+  '5A0007', '809693', 'FEFFE6', '1B4400', '4FC601', '3B5DFF', '4A3B53', 'FF2F80',
+  '61615A', 'BA0900', '6B7900', '00C2A0', 'FFAA92', 'FF90C9', 'B903AA', 'D16100',
+  'DDEFFF', '000035', '7B4F4B', 'A1C299', '300018', '0AA6D8', '013349', '00846F',
+  '372101', 'FFB500', 'C2FFED', 'A079BF', 'CC0744', 'C0B9B2', 'C2FF99', '001E09',
+  '00489C', '6F0062', '0CBD66', 'EEC3FF', '456D75', 'B77B68', '7A87A1', '788D66',
+  '885578', 'FAD09F', 'FF8A9A', 'D157A0', 'BEC459', '456648', '0086ED', '886F4C',
+  '34362D', 'B4A8BD', '00A6AA', '452C2C', '636375', 'A3C8C9', 'FF913F', '938A81',
+  '575329', '00FECF', 'B05B6F', '8CD0FF', '3B9700', '04F757', 'C8A1A1', '1E6E00',
+  '7900D7', 'A77500', '6367A9', 'A05837', '6B002C', '772600', 'D790FF', '9B9700',
+  '549E79', 'FFF69F', '201625', '72418F', 'BC23FF', '99ADC0', '3A2465', '922329',
+  '5B4534', 'FDE8DC', '404E55', '0089A3', 'CB7E98', 'A4E804', '324E72', '6A3A4C', '000000',
+];
+
 exports.matchingAnswers = functions.https.onCall(({ regex }) =>
   words.filter(word => word.match(regex)));
 
-exports.decorateCursor = functions.database.ref('/cursors/{crosswordId}/{cursorId}').onCreate((snapshot, context) =>
-  admin.auth().getUser(context.auth.uid).then(user =>
-    snapshot.ref.update({
-      displayName: user.displayName,
-      photoUrl: user.photoURL,
-    })));
-
 const snapVal = snap => snap.val();
+
+exports.decorateCursor = functions.database.ref('/cursors/{crosswordId}/{cursorId}').onCreate((snapshot, context) =>
+  snapshot.ref.parent.once('value')
+    .then(snapVal)
+    .then(cursors => Object.values(cursors || {}))
+    .then(values => values.map(value => value.color))
+    .then(usedColors => colors.find(color => !usedColors.includes(color)) || colors[Math.floor(Math.random(0, colors.length))])
+    .then(color =>
+      admin.auth().getUser(context.auth.uid).then(user =>
+        snapshot.ref.update({
+          displayName: user.displayName,
+          photoUrl: user.photoURL,
+          color,
+        }))));
 
 exports.finishCommunalCrossword = functions.https.onCall(() =>
   admin.database().ref('/communalCrossword/current').once('value').then(snapVal)
