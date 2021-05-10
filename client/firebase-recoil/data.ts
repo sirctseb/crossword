adcdae27964ba6999deaa8eb0b2b7f9c9c37a17c
Christopher Best <bestchris@gmail.com>
2021-05-09 22:13:38 -0400

    add firebase data file a long time ago

---
+++ client/firebase-recoil/data.ts
@@ -0,0 +1,35 @@
+import { makeAtomFamily, PathParameters } from './static';
+
+export enum Direction {
+  across = 'across',
+  down = 'down',
+}
+
+type Matrix<P> = P[][];
+
+export interface Box {
+  blocked?: boolean;
+  circled?: boolean;
+  shaded?: boolean;
+  content?: string;
+}
+
+export interface Crossword {
+  rows: number;
+  symmetric: boolean;
+  // TODO any object or array-valued field in bolt is implicitly nullable -> optional
+  // need to study and confirm
+  themeEntries?: Record<string, boolean>;
+  clues?: Record<Direction, Matrix<string>>;
+  // TODO we don't actually know if this is a Box[][] or a Record<Record<number, Box>>
+  // and we are suffering from it because they are often interchangeable in client code but not by type
+  // maybe this is something we normalize
+  boxes?: Matrix<Box>;
+  title?: string;
+}
+
+interface CrosswordsParams extends PathParameters {
+  crosswordId: string;
+}
+
+export const Crosswords = makeAtomFamily<Crossword, CrosswordsParams>('/crosswords/{crosswordId}');
