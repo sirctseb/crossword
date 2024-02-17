d9d517d184a989a1c3fd6886907ffaf68fb0d399
Christopher Best <bestchris@gmail.com>
2024-02-16 21:53:20 -0500

    show login ui

--- new/src/app/page.tsx
+++ new/src/app/page.tsx
@@ -1,21 +1,27 @@
-"use client"
+"use client";
 
 import Image from "next/image";
-import { useRecoilValue} from 'recoil'
-import { getFirebaseApp } from "../firebase";
-import { makeAuthAtom } from "../firebase-recoil/auth";
+import { useRecoilValue } from "recoil";
 
-const app = getFirebaseApp()
-const authAtom = makeAuthAtom(app)
+import {
+  firebaseAuthConfig,
+  getFirebaseApp,
+  getFirebaseAuth,
+} from "../firebase";
+import { makeAuthAtom } from "../firebase-recoil/auth";
+import StyledFirebaseAuth from "../firebase/ui/react-firebaseui";
+
+const app = getFirebaseApp();
+const auth = getFirebaseAuth();
+const authAtom = makeAuthAtom(app);
 
 export default function Home() {
-  const auth =  useRecoilValue(authAtom)
+  const authState = useRecoilValue(authAtom);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <pre>
-        {JSON.stringify(auth, undefined, '\t')}
-      </pre>
+      <pre>{JSON.stringify(auth, undefined, "\t")}</pre>
+      <StyledFirebaseAuth firebaseAuth={auth} uiConfig={firebaseAuthConfig} />
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
--- new/src/firebase/index.ts
+++ new/src/firebase/index.ts
@@ -1,6 +1,41 @@
-import { initializeApp } from "firebase/app";
+import {
+  Auth,
+  getAuth,
+  EmailAuthProvider,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+  TwitterAuthProvider,
+} from "firebase/auth";
+import { FirebaseApp, initializeApp } from "firebase/app";
 import { settings } from "../settings/Settings";
 
-export function getFirebaseApp() {
-  return initializeApp(settings.firebase);
+const globalFirebase = initializeApp(settings.firebase);
+
+export function getFirebaseApp(): FirebaseApp {
+  return globalFirebase;
 }
+
+export function getFirebaseAuth(): Auth {
+  return getAuth(globalFirebase);
+}
+
+export const firebaseAuthConfig = {
+  autoUpgradeAnonymousUsers: true,
+  callbacks: {
+    uiShown: () => {
+      // TODO ? example code hides a loader
+    },
+  },
+  signInFlow: "popup",
+  // TODO can we come back to whatever the current url is?
+  signInSuccessUrl: "/",
+  signInOptions: [
+    EmailAuthProvider.PROVIDER_ID,
+    GoogleAuthProvider.PROVIDER_ID,
+    FacebookAuthProvider.PROVIDER_ID,
+    TwitterAuthProvider.PROVIDER_ID,
+  ],
+  // TODO what are these used for?
+  tosUrl: "/tos",
+  privacyPolicyUrl: "/privacy",
+};
