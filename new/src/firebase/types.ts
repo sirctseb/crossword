58011783a1d02cf856799383bd2320d29ca1f717
Christopher Best <bestchris@gmail.com>
2024-02-17 15:19:24 -0500

    add type generation

---
+++ new/src/firebase/types.ts
@@ -0,0 +1,37 @@
+export type FirebaseArray<K extends string, T> = Record<K, T> | T[];
+export type Matrix<Type> = FirebaseArray<Index, FirebaseArray<Index, Type>>;
+export type Presence<Type extends string> = FirebaseArray<Type, boolean>;
+export type Crossword = {
+  rows: number;
+  symmetric: boolean;
+  themeEntries?: FirebaseArray<string, boolean>;
+  clues?: FirebaseArray<Direction, Matrix<string>>;
+  boxes?: Matrix<Box>;
+  title?: string;
+};
+export type Index = string;
+export type Direction = string;
+export type Box = {
+  blocked?: boolean;
+  circled?: boolean;
+  shaded?: boolean;
+  content?: string;
+};
+export type CrosswordMetadata = {
+  title?: string;
+};
+export type WordlistEntry = {
+  word: string;
+  usedBy?: Presence<CrosswordId>;
+};
+export type User = {
+  crosswords?: FirebaseArray<string, CrosswordMetadata>;
+  wordlist?: FirebaseArray<string, WordlistEntry>;
+};
+export type Permissions = {
+  owner: UserId;
+  collaborators?: Presence<UserId>;
+  global?: boolean;
+};
+export type UserId = string;
+export type CrosswordId = string;
