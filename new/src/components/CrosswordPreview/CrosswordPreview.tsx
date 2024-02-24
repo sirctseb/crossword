749bee748a930e67ccbb4d096b70a456f9479dc5
Christopher Best <bestchris@gmail.com>
2024-02-23 23:07:28 -0500

    move crossword reshape to a selector

--- new/src/components/CrosswordPreview/CrosswordPreview.tsx
+++ new/src/components/CrosswordPreview/CrosswordPreview.tsx
@@ -1,43 +1,26 @@
 import React from "react";
 import { block } from "../../styles";
 
-import {
-  Crossword,
-  CrosswordMetadata,
-  FirebaseArray,
-} from "../../firebase/types";
-import { coerceToArray, makeAtomFamily } from "../../firebase-recoil";
+import { Crossword, CrosswordMetadata } from "../../firebase/types";
 
 import "./crossword-preview.scss";
 import { useRecoilValue } from "recoil";
-import { getFirebaseDatabase } from "../../firebase";
+import {
+  arrayCrosswordFamily,
+  type ArrayCrossword,
+} from "../../firebase-recoil/atoms";
 
 const bem = block("crossword-preview");
 
 interface CrosswordPreviewProps {
   id: string;
-  crossword: Crossword;
+  crossword: ArrayCrossword;
   metadata: CrosswordMetadata;
 }
 
-// alternative approach would be to have an access utility like get
-// to gracefully degrade to null / default value on value absence. lib could
-// provide that also.
-const coerceMatrixToArray = <T,>(
-  matrix: FirebaseArray<string, FirebaseArray<string, T>>,
-  defaultValue: T,
-  rows: number,
-  columns: number
-): T[][] => {
-  const outer = coerceToArray(matrix, {}, rows);
-  return outer.map((inner) => coerceToArray(inner, defaultValue, columns));
-};
-
-const Boxes: React.FC<{ rows: number; boxes?: Crossword["boxes"] }> = ({
-  rows,
-  boxes: originalBoxes,
+const Boxes: React.FC<{ rows: number; boxes: ArrayCrossword["boxes"] }> = ({
+  boxes,
 }) => {
-  const boxes = coerceMatrixToArray(originalBoxes ?? [], {}, rows, rows);
   return boxes.map((boxesRow, row) => (
     <div key={`row-${row}`} className={bem("row")}>
       {boxesRow.map((box, column) => (
@@ -74,18 +57,13 @@ export interface ConnectedCrosswordPreviewProps {
   metadata: CrosswordMetadata;
 }
 
-const crosswordAtomFamily = makeAtomFamily<Crossword, { crosswordId: string }>(
-  "/crosswords/{crosswordId}",
-  getFirebaseDatabase()
-);
-
 export const ConnectedCrosswordPreview: React.FC<
   ConnectedCrosswordPreviewProps
 > = ({ id, metadata }) => {
   // TODO how does this work if the value isn't already loaded? there's no
   // typing here about promises, looks like we always get it on first render
   // but that can't be true
-  const crossword = useRecoilValue(crosswordAtomFamily({ crosswordId: id }));
+  const crossword = useRecoilValue(arrayCrosswordFamily({ crosswordId: id }));
 
   return <CrosswordPreview id={id} metadata={metadata} crossword={crossword} />;
 };
--- new/src/firebase-recoil/atoms.ts
+++ new/src/firebase-recoil/atoms.ts
@@ -1,4 +1,50 @@
 import { makeAuthAtom } from "./auth";
-import { getFirebaseApp } from "../firebase";
+import { getFirebaseApp, getFirebaseDatabase } from "../firebase";
+import { makeAtomFamily } from ".";
+import { coerceToArray } from "./";
+import type { Box, Crossword, FirebaseArray } from "../firebase/types";
+import { selectorFamily } from "recoil";
 
 export const authAtom = makeAuthAtom(getFirebaseApp());
+
+export const crosswordAtomFamily = makeAtomFamily<
+  Crossword,
+  { crosswordId: string }
+>("/crosswords/{crosswordId}", getFirebaseDatabase());
+
+export type ArrayCrossword = Omit<Crossword, "boxes"> & { boxes: Box[][] };
+
+// alternative approach would be to have an access utility like get
+// to gracefully degrade to null / default value on value absence. lib could
+// provide that also. we may have to revisit this if these selectors turn
+// out to be slow
+const coerceMatrixToArray = <T>(
+  matrix: FirebaseArray<string, FirebaseArray<string, T>>,
+  defaultValue: T,
+  rows: number,
+  columns: number
+): T[][] => {
+  const outer = coerceToArray(matrix, {}, rows);
+  return outer.map((inner) => coerceToArray(inner, defaultValue, columns));
+};
+
+export const arrayCrosswordFamily = selectorFamily<
+  ArrayCrossword,
+  { crosswordId: string }
+>({
+  key: `firebase-recoil:crossword-retyped`,
+  get:
+    (param) =>
+    async ({ get }) => {
+      const crossword = get(crosswordAtomFamily(param));
+      return {
+        ...crossword,
+        boxes: coerceMatrixToArray(
+          crossword.boxes ?? [],
+          {},
+          crossword.rows,
+          crossword.rows
+        ),
+      };
+    },
+});
