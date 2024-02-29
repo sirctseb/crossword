da962c27d141361766c7f5a36c68f88b21f80b32
Christopher Best <bestchris@gmail.com>
2024-02-28 22:35:58 -0500

    remove window guard on crossword page

--- new/src/app/[crosswordId]/page.tsx
+++ new/src/app/[crosswordId]/page.tsx
@@ -1,13 +1,10 @@
 import { ConnectedEditor } from "../../components/Editor/Editor";
 
-export default function UserDashboard({
+export default function CrosswordEditor({
   params: { crosswordId },
 }: {
   params: { crosswordId: string };
 }) {
-  if (typeof window === "undefined") {
-    return null;
-  }
   return <ConnectedEditor crosswordId={crosswordId} />;
 }
 // I love Mamma and Pappa
