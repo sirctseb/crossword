411383f64774898592b3f97ef67e183a4aefb769
Christopher Best <bestchris@gmail.com>
2024-03-08 20:57:52 -0500

    delete legacy implementation

--- legacy/src/editor/constants.js
+++
@@ -1,2 +0,0 @@
-export const ACROSS = 'across';
-export const DOWN = 'down';
--- legacy/src/user/UserSection.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import bem from 'bem-names';
-
-export default ({ children }) => (
-    <div className={bem('user-section')}>
-        {children}
-    </div>
-);
-
