4a6584d306aefda3021e3508e55b83f389805864
Christopher Best <bestchris@gmail.com>
2024-03-14 21:00:07 -0400

    improve auth types

--- src/firebase-recoil/auth.ts
+++ src/firebase-recoil/auth.ts
@@ -2,19 +2,34 @@ import { atom, AtomEffect, RecoilState } from "recoil";
 import { Auth, getAuth, User } from "firebase/auth";
 import { FirebaseApp } from "firebase/app";
 
-interface AuthState {
-  isLoaded: boolean;
-  isEmpty: boolean;
-  user: User | null;
+export interface UnloadedAuth {
+  isLoaded: false;
+  isEmpty: true;
 }
+
+interface LoadedEmptyAuth {
+  isLoaded: true;
+  isEmpty: true;
+}
+
+interface LoadedAuth {
+  isLoaded: true;
+  isEmpty: false;
+  user: User;
+}
+
+export type AuthState = UnloadedAuth | LoadedEmptyAuth | LoadedAuth;
 
 function fbAuthSubscriptionEffect(auth: Auth): AtomEffect<AuthState> {
   return ({ setSelf }) => {
     // TODO look through react-redux-firebase for more sophisticated
     // auth / profile watching
     return auth.onAuthStateChanged((user) => {
+      if (!user) {
+        return setSelf({ isLoaded: true, isEmpty: true });
+      }
       setSelf({
-        isEmpty: !user,
+        isEmpty: false,
         isLoaded: true,
         // if you pass the user object directly here, recoil, in development mode,
         // freezes the object passed in, which breaks things like subsequent calls
@@ -32,11 +47,15 @@ export function makeAuthAtom(app: FirebaseApp): RecoilState<AuthState> {
   return atom<AuthState>({
     key: `firebase-recoil-app:auth`,
     effects: [fbAuthSubscriptionEffect(auth)],
-    default: {
-      isEmpty: !auth.currentUser,
-      // TODO is there a synchronous way to get initial value for empty / loaded?
-      isLoaded: false,
-      user: auth.currentUser,
-    },
+    default: !!auth.currentUser
+      ? {
+          isEmpty: false,
+          isLoaded: true,
+          user: auth.currentUser,
+        }
+      : {
+          isEmpty: true,
+          isLoaded: false,
+        },
   });
 }
