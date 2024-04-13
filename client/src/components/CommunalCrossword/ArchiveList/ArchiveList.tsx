317653eca835679d2298cad90c709393790f63b9
Christopher Best <bestchris@gmail.com>
2024-04-12 20:16:51 -0400

    add current teaser and styling

--- client/src/components/CommunalCrossword/ArchiveList/ArchiveList.tsx
+++ client/src/components/CommunalCrossword/ArchiveList/ArchiveList.tsx
@@ -2,18 +2,33 @@ import { block } from "../../../styles";
 import { ConnectedCrosswordPreview } from "../../CrosswordPreview";
 
 const bem = block("archive-list");
+import "./archive-list.scss";
 
 interface ArchiveListProps {
   archiveList: string[];
+  onCurrentClick: () => void;
+  current: string;
 }
 
-export const ArchiveList: React.FC<ArchiveListProps> = ({ archiveList }) => {
+export const ArchiveList: React.FC<ArchiveListProps> = ({
+  archiveList,
+  onCurrentClick,
+  current,
+}) => {
   return (
     <div className={bem()}>
       <div className={bem("list")}>
         {archiveList.map((crosswordId) => (
           <ConnectedCrosswordPreview id={crosswordId} key={crosswordId} />
         ))}
+      </div>
+      <div className={bem("current")} onClick={onCurrentClick}>
+        <div className={bem("relative-reset")}>
+          <div className={bem("current-actuator")}>&gt;</div>
+          <div className={bem("previews")}>
+            <ConnectedCrosswordPreview id={current} />
+          </div>
+        </div>
       </div>
     </div>
   );
--- client/src/components/CommunalCrossword/index.tsx
+++ client/src/components/CommunalCrossword/index.tsx
@@ -1,21 +1,37 @@
 import { useRecoilValue } from "recoil";
 import { communalCrosswordAtom } from "../../firebase-recoil/atoms";
 import { block } from "../../styles";
-import { useMemo } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { ArchiveList } from "./ArchiveList";
 
 const bem = block("communal-crossword");
 
+enum Selection {
+  None = "None",
+  Current = "Current",
+}
+
 export const CommunalCrossword: React.FC = () => {
+  const [selectedCrossword, setSelectedCrossword] = useState<string>(
+    Selection.Current
+  );
   const { current, archive } = useRecoilValue(communalCrosswordAtom);
 
   // TODO selector?
   const archiveList = useMemo(() => Object.values(archive || {}), [archive]);
 
+  const onCurrentClick = useCallback(() => {
+    setSelectedCrossword(current);
+  }, [current]);
+
   return (
     <div className={bem()}>
       <h2>Communal Crossword</h2>
-      <ArchiveList archiveList={archiveList} />
+      <ArchiveList
+        archiveList={archiveList}
+        current={current}
+        onCurrentClick={onCurrentClick}
+      />
     </div>
   );
 };
