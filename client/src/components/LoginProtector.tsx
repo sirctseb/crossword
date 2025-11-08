f88260d685d15099de022cdd14e26c469c0cac2b
Christopher Best <bestchris@gmail.com>
2025-11-08 13:05:09 -0500

    refactor: move login protector off recoil

--- client/src/components/LoginProtector.tsx
+++ client/src/components/LoginProtector.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import { useRecoilValue } from "recoil";
-import { authAtom } from "../firebase-recoil/atoms";
+import { useAuth } from "../firebase";
 
 // TODO this might make SSR even harder? I guess we'll need an arbitrary
 // solution for priming atom state anyway, including waiting on auth
 export const LoginProtector = ({ children }: { children: React.ReactNode }) => {
-  // should we, instead, move this knowledge into firebase-recoil?
-  const auth = useRecoilValue(authAtom);
+  const auth = useAuth();
 
   if (auth.isEmpty) {
     return <div>loading...</div>;
--- client/src/firebase/useFirebase.ts
+++ client/src/firebase/useFirebase.ts
@@ -1,6 +1,7 @@
 import type { FirebaseApp } from "firebase/app";
-import type { Auth } from "firebase/auth";
+import type { Auth, User } from "firebase/auth";
 import type { Database } from "firebase/database";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useMemo } from "react";
 import { getFirebaseApp, getFirebaseAuth, getFirebaseDatabase } from ".";
 
@@ -19,4 +20,42 @@ export const useFirebase = (): UseFirebaseResult => {
     }),
     []
   );
+};
+
+export interface UnloadedAuth {
+  isLoaded: false;
+  isEmpty: true;
+}
+
+interface LoadedEmptyAuth {
+  isLoaded: true;
+  isEmpty: true;
+}
+
+interface LoadedAuth {
+  isLoaded: true;
+  isEmpty: false;
+  user: User;
+}
+
+export type AuthState = UnloadedAuth | LoadedEmptyAuth | LoadedAuth;
+
+export const useAuth = (): AuthState => {
+  const [user, loading, error] = useAuthState(getFirebaseAuth());
+
+  if (loading) {
+    return { isLoaded: false, isEmpty: true };
+  }
+
+  if (user) {
+    return {
+      isLoaded: true,
+      isEmpty: false,
+      user,
+    };
+  }
+  return {
+    isLoaded: true,
+    isEmpty: true,
+  };
 };
