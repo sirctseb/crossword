605d39b95957b2c6d621d62324ed0c54f0f40f46
Christopher Best <bestchris@gmail.com>
2025-11-14 23:55:23 -0500

    feat: add crossword atom stack

--- client/src/state/atoms/firebaseAtoms.ts
+++ client/src/state/atoms/firebaseAtoms.ts
@@ -1,10 +1,24 @@
 import { getDatabase } from "firebase/database";
-import type { CommunalCrossword, Cursor, User } from "../../firebase/types";
+import type {
+  CommunalCrossword,
+  Crossword,
+  Cursor,
+  Matrix,
+  User,
+} from "../../firebase/types";
 import { makeAtom, makeAtomFamily } from "../../jotai-firebase";
 import { atomFamily } from "jotai/utils";
 import deepEqual from "fast-deep-equal";
 import { atom, type Atom } from "jotai";
-import { type CursorMap, reduceCursors } from "../derivations";
+import {
+  type CursorMap,
+  deriveAllAnswers,
+  deriveArrayCrossword,
+  deriveClueAddresses,
+  deriveLabeledAddressMap,
+  reduceCursors,
+} from "../derivations";
+import type { ArrayCrossword, LabeledAddressCatalog } from "../types";
 
 const database = getDatabase();
 
@@ -56,5 +70,89 @@ export const remoteCursorAtomFamily = atomFamily<
 
     // const { [params.cursorId ?? ""]: _, ...remoteCursors } = cursors ?? {};
     // return reduceCursors(remoteCursors);
+  });
+}, deepEqual);
+
+export const crosswordAtomFamily = makeAtomFamily<
+  Crossword,
+  { crosswordId: string }
+>("/crosswords/{crosswordId}", database, {
+  clues: { across: {}, down: {} },
+  title: "skeleton title",
+  rows: 1,
+  symmetric: true,
+});
+
+// TODO we should make an atomFamily wrapper that uses deepEqual
+export const arrayCrosswordAtomFamily = atomFamily<
+  { crosswordId: string },
+  Atom<ArrayCrossword | Promise<ArrayCrossword>>
+>((params) => {
+  return atom((get) => {
+    const crossword = get(
+      crosswordAtomFamily({ crosswordId: params.crosswordId })
+    );
+    if (crossword instanceof Promise) {
+      return crossword.then((resolvedCrossword) => {
+        return deriveArrayCrossword(resolvedCrossword);
+      });
+    }
+    return deriveArrayCrossword(crossword);
+  });
+}, deepEqual);
+
+export const allAnswersAtomFamily = atomFamily<
+  { crosswordId: string },
+  Atom<string[] | Promise<string[]>>
+>((get) => {
+  return atom((getAtom) => {
+    const arrayCrossword = getAtom(
+      arrayCrosswordAtomFamily({ crosswordId: get.crosswordId })
+    );
+    if (arrayCrossword instanceof Promise) {
+      return arrayCrossword.then((resolvedArrayCrossword) => {
+        return deriveAllAnswers(resolvedArrayCrossword);
+      });
+    }
+    return deriveAllAnswers(arrayCrossword);
+  });
+}, deepEqual);
+
+export const labeledAddressCatalogAtomFamily = atomFamily<
+  { crosswordId: string },
+  Atom<LabeledAddressCatalog | Promise<LabeledAddressCatalog>>
+>((params) => {
+  return atom((get) => {
+    const crossword = get(
+      arrayCrosswordAtomFamily({ crosswordId: params.crosswordId })
+    );
+    if (crossword instanceof Promise) {
+      return crossword.then(deriveClueAddresses);
+    }
+    return deriveClueAddresses(crossword);
+  });
+}, deepEqual);
+
+export const labeledAddressMapAtomFamily = atomFamily<
+  {
+    crosswordId: string;
+  },
+  Atom<Matrix<number> | Promise<Matrix<number>>>
+>((params) => {
+  return atom((get) => {
+    return Promise.resolve(
+      get(labeledAddressCatalogAtomFamily({ crosswordId: params.crosswordId }))
+    ).then(deriveLabeledAddressMap);
+
+    // const labeledAddressCatalog = get(
+    //   labeledAddressCatalogAtomFamily({ crosswordId: params.crosswordId })
+    // );
+    // if (labeledAddressCatalog instanceof Promise) {
+    //   return labeledAddressCatalog.then((resolvedCatalog) => {
+    //     return deriveLabeledAddressMap(resolvedCatalog);
+    //   });
+    // }
+
+    // return deriveLabeledAddressMap(labeledAddressCatalog);
   });
 }, deepEqual);
