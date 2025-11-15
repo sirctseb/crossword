4db716bbdfe3b86447f95c26e409234895803cdf
Christopher Best <bestchris@gmail.com>
2025-11-15 08:11:21 -0500

    upgrade next, react, eslint

--- client/src/app/[crosswordId]/page.tsx
+++ client/src/app/[crosswordId]/page.tsx
@@ -1,10 +1,9 @@
 import { ConnectedEditor } from "../../components/Editor/Editor";
 
-export default function CrosswordEditor({
-  params: { crosswordId },
-}: {
-  params: { crosswordId: string };
-}) {
+export default async function CrosswordEditor(
+  props: PageProps<"/[crosswordId]">
+) {
+  const { crosswordId } = await props.params;
   return <ConnectedEditor crosswordId={crosswordId} />;
 }
 // I love Mamma and Pappa
